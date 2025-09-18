9bd0382dbb4c2e45cead2ca869fa56cf7aa314ef
andrezzaS <andrezzasantos090@gmail.com>
2025-09-18 18:55:39 -0300

    projeto

---
+++ .vscode/script.js
@@ -0,0 +1,18 @@
+function toggLeMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+
+  //pegar a tag img
+  const img = document.querySelector('#profile img')
+  //substituir a imagem
+  if (html.classList.contains('light')) {
+  // se tiver light mode, adicionar a imagem light
+   img.setAttribute('src', './Avatar.png')
+} else {
+// se tiver sem light mode, manter a imagem normal
+   img.setAttribute('src', './got.png')
+}
+
+
+}
